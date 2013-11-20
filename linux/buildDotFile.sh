4dadfd4156a5a5b0472b00331abcb8b8334e7ebb
bronze1man <bronze1man@gmail.com>
2013-11-20 14:28:35 +0800

    save

---
+++ linux/buildDotFile.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd ~/
+git clone git://github.com/bronze1man/dotfile 
+./dotfile/install.sh
---
+++ linux/installgo.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/../tmp
+wget https://go.googlecode.com/files/go1.2rc5.linux-amd64.tar.gz
+tar -xf go1.2rc5.linux-amd64.tar.gz
+sudo cp -rf go /usr/local/go
+if ! grep -q 'export GOROOT=/usr/local/go' '/etc/profile' ; then
+    echo "$(cat <<'EOF'
+export GOROOT=/usr/local/go
+export PATH=$PATH:$GOROOT/bin
+EOF
+    )" | sudo tee -a /etc/profile > /dev/null
+fi
+sudo apt-get install gcc
