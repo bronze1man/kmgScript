4fb243f04b1ec64827c6f02bd597a0066023e2c3
bronze1man <bronze1man@gmail.com>
2014-01-25 20:33:33 +0800

    update go version to 1.2

--- linux/installgo.sh
+++ linux/installgo.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+# if you are in china,please put "203.208.48.145 go.googlecode.com" to /etc/hosts
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 ./cleanTmp.sh
 cd $DIR/../tmp
-wget --no-check-certificate https://go.googlecode.com/files/go1.2rc5.linux-amd64.tar.gz
-tar -xf go1.2rc5.linux-amd64.tar.gz
+wget --no-check-certificate https://go.googlecode.com/files/go1.2.src.tar.gz
+tar -xf go1.2.src.tar.gz
 sudo cp -rf go /usr/local/go
 if ! grep -q 'export GOROOT=/usr/local/go' '/etc/profile' ; then
     echo "$(cat <<'EOF'
@@ -13,3 +15,5 @@ EOF
     )" | sudo tee -a /etc/profile > /dev/null
 fi
 sudo apt-get install gcc
+cd /usr/local/go/src
+sudo ./make.bash
